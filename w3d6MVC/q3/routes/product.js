ae2e51358ddf56f2bad77a951fc44bcda44551c2
Alvin Abaho <abahoalvin@gmail.com>
2021-07-11 21:24:21 -0500

    [REFACTOR] splits controller logic from route logic

--- w3d6MVC/q3/routes/product.js
+++ w3d6MVC/q3/routes/product.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const router = express.Router();
-const Product = require('../models/product');
+const controller = require('../controllers/product');
 
-router.get('/', (req, res) => {
-    const product = Product.getRandomProduct();
-    res.render('product', {product});
-});
+const router = express.Router();
+
+router.get('/', controller.getRandomProduct);
 
 module.exports = router;
--- w3d6MVC/q4/routes/product.js
+++ w3d6MVC/q4/routes/product.js
@@ -1,21 +1,10 @@
 const express = require('express');
+const controller = require('../controllers/product');
+
 const router = express.Router();
-const Product = require('../models/product');
 
-router.get('/', (req, res) => {
-    const product = Product.getRandomProduct();
-    res.render('product', {product});
-});
-
-router.post('/addToCart', (req, res) => {
-    Product.addToCart(req.body.id);
-    res.redirect(303, '/cart');
-});
-
-router.get('/cart', (req, res) => {
-    const cart = Product.getCart();
-    const totalPrice = cart.map(order => order.product.price * order.qty).reduce((a, b) => a + b, 0);
-    res.render('shoppingCart', {cart, totalPrice: totalPrice.toFixed(2)});
-});
+router.get('/', controller.getRandomProduct);
+router.post('/addToCart', controller.addToCart);
+router.get('/cart', controller.getCart);
 
 module.exports = router;
